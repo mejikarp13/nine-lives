cb4005c4b673667db8da090643f5f4b6086fda6d
mejikarp13 <finandnitro@gmail.com>
2025-04-12 15:18:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     updateDisplay();
 });
+
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+
